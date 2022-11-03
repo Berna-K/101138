a78bbf042db600d71ff8b7f1422f2dbcdc1e26bd
ben <kimaryobernard@gmail.com>
2022-11-03 19:22:16 +0300

    non

--- Question2/Temperature.cpp
+++ Question2/Temperature.cpp
@@ -8,10 +8,10 @@ float Farenheit =0;
 
 //conversion formula
 float Celsius = 5.0/9.0*(Farenheit-32.0);
-
+//input request
 cout<<"Please enter your temperature in degrees farenheit "<<endl;
 cin>>Farenheit;
-
+//output
 cout<<"Your temperature in celsius is equal to "<<Celsius<<endl;
 return 0;
 
