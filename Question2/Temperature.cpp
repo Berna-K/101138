f17a8a77a608d63f0f33401826f60823392d2dc8
ben <kimaryobernard@gmail.com>
2022-11-03 19:24:02 +0300

    cat 1

--- Question2/Temperature.cpp
+++ Question2/Temperature.cpp
@@ -11,7 +11,7 @@ float Celsius = 5.0/9.0*(Farenheit-32.0);
 //input request
 cout<<"Please enter your temperature in degrees farenheit "<<endl;
 cin>>Farenheit;
-//output
+//output.
 cout<<"Your temperature in celsius is equal to "<<Celsius<<endl;
 return 0;
 
